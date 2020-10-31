35e8efa930d1106c7df2f93b99ee682ae60203e9
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-31 21:34:07 +0100

    Still playing around react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Hello from './sayHello';
+import Tweet from './Tweet';
+
 
 function App(){
 
   return(
-    <div>
-      <h1>This is the app component</h1>
-      <Hello/>
+    <div className="app">
+      <h1>Hello Reformers...</h1>
+      <Tweet/>
     </div>
   );
 }
