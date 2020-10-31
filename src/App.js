42e7522de913088e4249066c3c648909258033a8
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-11-01 00:23:43 +0100

    A counter is built with react state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 
 function App(){
 
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return(
     <div className="app">
-      <Tweet name="wizard calidad"/> 
-      <Tweet name="Qoyum Yusuf"/> 
-      <Tweet name="Splendour Olatunde"/>
-      <Tweet name="Galibah Yusuf"/>  
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function Tweet(props){
+function Tweet({name, message, likes}){
     return(
        <div className="tweet">
-            <h3> {props.name} </h3>
+            <h3> {name} </h3>
+            <p>{message}</p>
             <p> This is a foolish tweet. </p>
-            <h3> Number of likes </h3>
+            <h3> Number of likes {likes} </h3>
         </div>
     );
 }
