2c6ee207f2be79bbc7f21fe07f5f4c3d3f31f46e
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-11-15 22:15:05 +0100

    new joblib models added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Tweet from './components/Tweet';
-import Result from './components/Result';
 import MyNav from './navbar';
 import Footer from './footer';
 import toaster from "toasted-notes";
@@ -31,7 +30,7 @@ function App(){
     e.preventDefault();
     setLoading(true);
 
-      fetch('https://hate-speech-dev.herokuapp.com/predict', {method: "post", body: JSON.stringify(data), headers:{'Content-Type': "application/json"}})
+      fetch('https://hatespeechbot.herokuapp.com/predict', {method: "post", body: JSON.stringify(data), headers:{'Content-Type': "application/json"}})
       .then(response => response.text())
       .then(res => toaster.notify(res, {duration: 5000}))
       .catch((err) => console.log(err))
@@ -50,7 +49,6 @@ function App(){
        <div className="app">
       <h1>OLÓÒTỌ́- is a platform where you can analyze tweets and product reviews to derive insights of bad comments, hate speech or an acknowledgement...</h1>
       <Tweet handleChange={handleChange} handleSubmit={handleSubmit} name="Analyze" models="{models}"/>
-      <Result name="Analyze All Models" models="{models}"/>
       
     </div>
     <Footer />
